4e9ed15e632f83a3e4872ef7b6b9e1dd0636b45a
sntwandy <sntwandy@gmail.com>
2020-07-01 19:39:10 -0400

    Initializing the project

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { greeting } from './js/components';
+
+const megan = 'Megan E.'
+
+greeting(megan);
---
+++ src/js/components.js
@@ -0,0 +1,7 @@
+const greeting = ( name ) => {
+    console.log('Creating h1 etiquet');
+    const h1 = document.createElement('h1');
+    h1.innerHTML = `Hi, ${name}`;
+
+    document.body.append(h1);
+};
